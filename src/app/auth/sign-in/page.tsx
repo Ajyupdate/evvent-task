296a530bd6fb73ee3dddf348e5a467e8f5672789
Ajibade <ajibadeemmanuel58@gmail.com>
2023-10-05 15:18:57 +0100

    statistics chart

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -5,7 +5,7 @@ import PageLayout from "@/modules/dashboard/layout/DashboardLayout";
 export default function SignIn() {
   return (
     <PageLayout title="Sign In">
-      <div>"Sign in page"</div>
+      <div>Sign in page</div>
     </PageLayout>
   );
 }
